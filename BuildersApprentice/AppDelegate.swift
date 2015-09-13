0945449ac6089a28992b9fa7c27f7ce1ef0d7430
Steve Baker <tektsu@gmail.com>
2015-09-13 14:01:09 -0700

    Initial Commit

---
+++ BuildersApprentice/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BuildersApprentice
+//
+//  Created by Steve Baker on 9/13/15.
+//  Copyright © 2015 Steve Baker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
